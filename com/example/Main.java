0b9d031823a249eeaffecd2f87de690e380e990c
DeepankRx <deepankpushpad10@gmail.com>
2022-03-10 16:12:09 +0530

    first commit

---
+++ com/example/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Number :");
+        int num = scanner.nextInt();
+	    System.out.println("Factorial Is : "+ fact(num));
+    }
+   static int fact(int num)
+    {
+        if(num == 1 || num == 0 )
+        {
+            return 1;
+        }
+        return num * fact(--num);
+    }
+}
